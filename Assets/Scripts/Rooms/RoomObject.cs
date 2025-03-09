b88b9a1337883030fca123deee2ce7d5171a89e1
Blomman <johnny@geoguessr.com>
2025-03-09 09:56:24 +0100

    Disabling room objects in Awake if enabled with rooms

--- Assets/Scripts/Rooms/RoomObject.cs
+++ Assets/Scripts/Rooms/RoomObject.cs
@@ -1,11 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Rooms
 {
     public class RoomObject : MonoBehaviour, IRoomObject
     {
         public bool enableWithRoom = true;
-        
+
+        private void Awake()
+        {
+            if (!enableWithRoom) return;
+            gameObject.SetActive(false);
+        }
+
         public void OnRoomEnter(RoomData data)
         {
             if (!enableWithRoom) return;
