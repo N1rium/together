2ea1d358e0b1176010d000bfe0e207801ac2aca1
Blomman <johnny@geoguessr.com>
2025-03-12 20:19:25 +0100

    Nullcheck moving platform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -12,6 +12,7 @@ public class MovingPlatform : MonoBehaviour
 
     public void Update()
     {
+        if (NetworkManager.Singleton == null) return;
         // Move up and down by 5 meters and change direction every 3 seconds.
         var positionY = Mathf.PingPong(NetworkManager.Singleton.LocalTime.TimeAsFloat / 3f, 1f) * 5f;
         transform.position = startPos + new Vector3(0, positionY, 0);
